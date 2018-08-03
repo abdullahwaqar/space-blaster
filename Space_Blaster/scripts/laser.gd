41487e4cb5dccb751c5c1beeeceb66f4a826dc57
awr417h <abdullah@aw.com>
2018-08-04 04:36:14 +0500

    shoot()

---
+++ Space_Blaster/scripts/laser.gd
@@ -0,0 +1,33 @@
+extends Area2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+#[x] apply velocity
+#[x] instantiate a flare
+#[-] self-destroy when out of screen
+
+export var velocity = Vector2()
+const scn_flare = preload('res://scenes/flare.tscn')
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	set_process(true)
+	create_flare()
+	
+	yield(get_node('vis_notifier'), 'tree_exited')
+	pass
+
+func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+	translate(velocity * delta)
+	pass
+
+func create_flare():
+	var flare = scn_flare.instance()
+	flare.position = position
+	utils.main_node.add_child(flare)
+	pass
--- Space_Blaster/scripts/ship.gd
+++ Space_Blaster/scripts/ship.gd
@@ -4,10 +4,14 @@ extends Area2D
 # var a = 2
 # var b = "textvar"
 
+const scn_laser = preload('res://scenes/laser_ship.tscn')
+
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	set_process(true)
+	yield(utils.create_timer(0.5), 'timeout')
+	shoot()
 	pass
 
 func _process(delta):
@@ -21,4 +25,19 @@ func _process(delta):
 	var pos = position
 	pos.x = clamp(pos.x, 0 + 16, view_size - 16) 
 	position = pos
+	pass
+
+func shoot():
+	while true:
+		var pos_left = get_node('cannons/left').global_position
+		var pos_right = get_node('cannons/right').global_position
+		create_laser(pos_left)
+		create_laser(pos_right)
+		yield(utils.create_timer(0.25), 'timeout')
+	pass
+
+func create_laser(pos):
+	var laser = scn_laser.instance()
+	laser.position = pos
+	utils.main_node.add_child(laser)
 	pass
